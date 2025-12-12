5b17499f5fed46a10da96cd612c0a2bbe040700f
monasaad1512 <monasaad221049@gmail.com>
2025-12-12 20:53:51 +0200

    update EditItemActivity

--- app/src/main/java/com/example/studentmate/Activities/EditItemActivity.kt
+++ app/src/main/java/com/example/studentmate/Activities/EditItemActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -16,8 +24,24 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +52,6 @@ import androidx.compose.ui.unit.sp
 import com.example.studentmate.ui.theme.StudentMateTheme
 
 
-
 data class EditItem(
     val id: Int = 0,
     val type: String,
@@ -36,7 +59,6 @@ data class EditItem(
     val date: String,
     val score: String,
     val description: String,
-    val notify: Boolean
 )
 
 class EditItemActivity : ComponentActivity() {
@@ -64,7 +86,6 @@ fun EditItemScreen() {
     var date by remember { mutableStateOf("") }
     var score by remember { mutableStateOf("0") }
     var description by remember { mutableStateOf("") }
-    var isNotificationEnabled by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -93,7 +114,6 @@ fun EditItemScreen() {
                             date = date,
                             score = score,
                             description = description,
-                            notify = isNotificationEnabled
                         )
                         // TODO:Save 'itemToSave' to the database
                     },
@@ -144,7 +164,6 @@ fun EditItemScreen() {
                 placeholder = "Add notes or details about this item"
             ) { description = it }
 
-            NotificationSwitch(isNotificationEnabled, selectedType) { isNotificationEnabled = it }
 
             Spacer(modifier = Modifier.height(60.dp))
         }
@@ -266,24 +285,3 @@ fun TypeButton(text: String, isSelected: Boolean, color: Color, modifier: Modifi
     }
 }
 
-@Composable
-fun NotificationSwitch(isEnabled: Boolean, type: String, onToggle: (Boolean) -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(1.dp, Color.LightGray.copy(0.5f), RoundedCornerShape(8.dp))
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column {
-            Text("Notify me before ${type.lowercase()}", fontWeight = FontWeight.Medium)
-            Text("Get reminders", style = MaterialTheme.typography.bodySmall, color = Color.Gray)
-        }
-        Switch(
-            checked = isEnabled,
-            onCheckedChange = onToggle,
-            colors = SwitchDefaults.colors(checkedTrackColor = Color(0xFF2196F3))
-        )
-    }
-}
